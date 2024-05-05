a2069841677d09b9c6b628616e44a25355b4138c
Emre Göktepe <146372845+emogoektepe@users.noreply.github.com>
2024-05-05 17:57:20 +0200

    employee interface

--- src/models/employee.ts
+++ src/models/employee.ts
@@ -1,15 +1,7 @@
-export class Employee {
+export interface Employee {
   id: string
   firstName: string
   lastName: string
   email: string
   address: string
-
-  constructor(obj?: any) {
-    this.id = obj ? obj.id : ''
-    this.firstName = obj ? obj.firstName : ''
-    this.lastName = obj ? obj.lastName : ''
-    this.email = obj ? obj.email : ''
-    this.address = obj ? obj.address : ''
-  }
 }
